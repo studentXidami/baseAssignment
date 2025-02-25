b157db4fb00338e9681896a1d257c78d59400b97
studentxidami <2767393435@qq.com>
2025-02-26 00:50:18 +0800

    cc

---
+++ computerAdd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        char op = char.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine(op == '+' ? a + b : op == '-' ? a - b : op == '*' ? a * b : a / b);
+    }
+}
