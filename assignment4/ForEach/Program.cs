a868716d14180bccbf11726a7278b87ac4c9156f
studentxidami <2767393435@qq.com>
2025-03-19 20:51:36 +0800

    x

---
+++ assignment4/ForEach/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+public class LinkedList<T>
+{
+    private class Node
+    {
+        public T Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    private Node head;
+
+    public void Add(T data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(10);
+        list.Add(5);
+        list.Add(20);
+        list.Add(3);
+        list.Add(15);
+
+        // 打印链表元素
+        Console.WriteLine("链表元素:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 求最大值
+        int max = int.MinValue;
+        list.ForEach(x => { if (x > max) max = x; });
+        Console.WriteLine($"最大值: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        list.ForEach(x => { if (x < min) min = x; });
+        Console.WriteLine($"最小值: {min}");
+
+        // 求和
+        int sum = 0;
+        list.ForEach(x => sum += x);
+        Console.WriteLine($"和: {sum}");
+    }
+}
---
+++ assignment4/clock/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Timers; // 明确使用 System.Timers 命名空间
+
+public class TickEventArgs : EventArgs
+{
+    public DateTime CurrentTime { get; set; }
+}
+
+public class AlarmEventArgs : EventArgs
+{
+    public DateTime AlarmTime { get; set; }
+}
+
+public class AlarmClock
+{
+    private readonly System.Timers.Timer _timer; 
+    public DateTime CurrentTime { get; private set; }
+    public DateTime AlarmTime { get; set; }
+
+    public event EventHandler<TickEventArgs> Tick;
+    public event EventHandler<AlarmEventArgs> Alarm;
+
+    public AlarmClock(DateTime initialTime)
+    {
+        CurrentTime = initialTime;
+        _timer = new System.Timers.Timer(1000); 
+        _timer.Elapsed += OnTimerElapsed;
+        _timer.AutoReset = true; 
+        _timer.Enabled = true;
+    }
+
+    private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        CurrentTime = CurrentTime.AddSeconds(1);
+        Tick?.Invoke(this, new TickEventArgs { CurrentTime = CurrentTime });
+
+        if (CurrentTime >= AlarmTime)
+        {
+            Alarm?.Invoke(this, new AlarmEventArgs { AlarmTime = AlarmTime });
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var alarmClock = new AlarmClock(new DateTime(2024, 1, 1, 8, 0, 0));
+        alarmClock.AlarmTime = alarmClock.CurrentTime.AddSeconds(5);
+
+        alarmClock.Tick += (sender, e) =>
+        {
+            Console.WriteLine($"Tick: {e.CurrentTime:HH:mm:ss}");
+        };
+
+        alarmClock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine($"\nALARM! Time reached: {e.AlarmTime:HH:mm:ss}");
+        };
+
+        Console.WriteLine("闹钟运行中，按任意键退出...");
+        Console.ReadKey();
+    }
+}
